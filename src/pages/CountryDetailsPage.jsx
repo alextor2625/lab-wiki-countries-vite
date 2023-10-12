ef1d7d6f169dc3d15f27fa3893813d691435725a
alextor26 <alextor2625@gmail.com>
2023-10-12 17:14:43 -0400

    lab done

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,54 @@
-function CountryDetails() {}
+import { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+function CountryDetails({country}) {
+
+  const { countryId } = useParams();
+
+  const [foundCountry, setFoundCountry] = useState(null)
+    console.log(country);
+//   const thisCountry = country.find(country => country.id === countryId)
+
+    return (
+
+        <div className="container">
+        <p style={{fontSize: '24px', fontWeight: 'bold'}}>Country Details</p>
+
+        <h1>Project details</h1>
+
+        <table className="table">
+          <thead></thead>
+          <tbody>
+            <tr>
+              <td style={{width: '30%'}}>Capital</td>
+              <td>Paris</td>
+            </tr>
+            <tr>
+              <td>Area</td>
+              <td>
+                551695 km
+                <sup>2</sup>
+              </td>
+            </tr>
+            <tr>
+              <td>Borders</td>
+              <td>
+                <ul>
+                  <li><Link to="/AND">Andorra</Link></li>
+                  <li><Link to="/BEL">Belgium</Link></li>
+                  <li><Link to="/DEU">Germany</Link></li>
+                  <li><Link to="/ITA">Italy</Link></li>
+                  <li><Link to="/LUX">Luxembourg</Link></li>
+                  <li><Link to="/MCO">Monaco</Link></li>
+                  <li><Link to="/ESP">Spain</Link></li>
+                  <li><Link to="/CHE">Switzerland</Link></li>
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
+}
 
 export default CountryDetails;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,53 @@
-function HomePage() {}
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function HomePage() {
+  const apiURL = "https://ih-countries-api.herokuapp.com/countries";
+
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    console.log("useEffect - Initial render (Mounting)");
+    axios
+      .get(apiURL)
+      .then((response) => {
+       // console.log(response.data);
+        setCountries(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, []);
+
+  return (
+    <div
+      className="container"
+      style={{ maxHeight: "90vh", overflow: "scroll" }}
+    >
+      <h1 style={{ fontSize: "24px" }}>
+        WikiCountries: Your Guide to the World
+      </h1>
+
+      <div className="list-group">
+        {countries.map((country) => {
+          return (
+            <Link
+              key={country._id}
+              className="list-group-item list-group-item-action"
+              to={{pathname: `/country/${country.alpha3Code.toLowerCase()}`, state: { country: country }}}
+            >
+
+              <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}></img>
+              
+              <p>{country.name.common}</p>
+              
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  )
+}
 
 export default HomePage;
